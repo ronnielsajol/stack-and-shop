edeab630d722a169c48371c47413b78b6066d8a3
Ronniel Daven Sajol <sajolronniel28@gmail.com>
2024-11-25 21:58:55 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Stack and Shop</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+  <header>
+    <h1>Welcome to Stack and Shop</h1>
+  </header>
+  <main>
+    <p>Boilerplateg</p>
+  </main>
+  <footer>
+    <p>&copy; <?php echo date("Y"); ?> Stack and Shop. All rights reserved.</p>
+  </footer>
+</body>
+
+</html>
